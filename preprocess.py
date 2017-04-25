66fae47c51d5306d2f7124768fdb227a20452923
mochiliu3000 <mochiliu3000@gmail.com>
2017-04-25 18:49:10 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -78,7 +78,7 @@ def extract_coords(train_path, train_dot_path, out_dir):
                     # get the color of the pixel from Train Dotted in the center of the blob
                     file.write("0" + " " + str(x_center) + " " + str(y_center) + " " + str(w_ratio) + " " + str(h_ratio) + "\n")
                     # file.write("0" + " " + str(x1) + " " + str(y1) + " " + str(x2) + " " + str(y2) + "\n")
-            if blob.size > 0:
+            if blob_num > 0:
                 tr_file.write(train_path + "/" + filename + ".JPEG\n")
 
 def convert_coord(x, y, w_img, h_img):
