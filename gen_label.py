20ad3711835f97e30d9969480451cb6e69dd479d
mochiliu3000 <mochiliu3000@gmail.com>
2017-05-27 14:58:18 +0800

    Add gen_less_female_nonzero_label

--- gen_label.py
+++ gen_label.py
@@ -1,8 +1,9 @@
 import os
+import operator
 
-label_dir = "/home/sleepywyn/Dev/GitRepo/Sealion/data/labels"
-image_dir = "/home/sleepywyn/Dev/GitRepo/Sealion/data/JPEGImages"
-out_dir = "/home/sleepywyn/Dev/GitRepo/Sealion/data"
+label_dir = "C:/Users/IBM_ADMIN/Desktop/Machine Learning/seaLion/data/labels"
+image_dir = "C:/Users/IBM_ADMIN/Desktop/Machine Learning/seaLion/data/JPEGImages"
+out_dir = "C:/Users/IBM_ADMIN/Desktop/Machine Learning/seaLion/data"
 
 def gen_nonzero_label(label_dir, image_dir, out_dir):
 	if not os.path.exists(label_dir):
@@ -14,6 +15,28 @@ def gen_nonzero_label(label_dir, image_dir, out_dir):
 				if num_lines > 0:
 					tr_file.write(image_dir + "/" + label_file[:-3] + "JPEG\n")
 
+					
+def gen_less_female_nonzero_label(label_dir, image_dir, out_dir, threshold):
+	if not os.path.exists(label_dir):
+		print("ERROR: The label folder you provided does not exists. Exit.")
+	else:
+		f_dict = {}
+		for label_file in os.listdir(label_dir):
+			labels = [line.split()[0] for line in open(label_dir + "/" + label_file)]
+			num_f_lines = sum([int(i == '3') for i in labels])
+			f_dict[label_file] = num_f_lines
+		sorted_f_dict = sorted(f_dict.items(), key=operator.itemgetter(1))
+		rm_dict_leng = int(round(len(sorted_f_dict) * threshold))
+		rm_dict = sorted_f_dict[-rm_dict_leng:]
+		print("Remove these images: ", rm_dict)
+		rm_imgs = [i[0] for i in rm_dict]
+		with open(out_dir + "/train_new.txt", "wb") as tr_file:
+			for label_file in os.listdir(label_dir):
+				num_lines = sum(1 for line in open(label_dir + "/" + label_file))
+				if num_lines > 0 and label_file not in rm_imgs:
+					tr_file.write(image_dir + "/" + label_file[:-3] + "JPEG\n")
+						
+
 def batch_rename(path):
 	for file_name in os.listdir(path):
 		os.rename(path + "/" + file_name, path + "/" + file_name[:-3] + "JPEG")
@@ -21,4 +44,5 @@ def batch_rename(path):
 
 if __name__ == '__main__':
 	# gen_nonzero_label(label_dir, image_dir, out_dir)
-	batch_rename("./data/Yolo_mark_result/JPEGImages")
+	# batch_rename("./data/Yolo_mark_result/JPEGImages")
+	gen_less_female_nonzero_label(label_dir, image_dir, out_dir, 0.3)
