84f950f322b7feda1ce0335ab035072c9d249a89
sleepywyn <sleepywyn@gmail.com>
2017-06-11 13:53:23 +0800

    add image resize option for generator

--- false_reduction_cnn.py
+++ false_reduction_cnn.py
@@ -10,15 +10,16 @@ from keras.layers import Input, Flatten, Dense
 from keras.models import Model
 from keras.optimizers import Adam
 
-img_size = 512
-
+img_size_width = 64
+img_size_height = 64
+resize_algorithm = Image.BILINEAR
 
 """
 generate sea lion images with given yolo format.
 imges should be in the folder: JPEGImages
 labels should be in the folder: labels
 """
-def sealion_gen(path_file):
+def sealion_gen(path_file, resize):
     ## end symbol is included in the line
     img_path_list = [line[:-1] for line in open(path_file)]
     shuffle(img_path_list)
@@ -39,12 +40,14 @@ def sealion_gen(path_file):
             half_h = origin_h * h_ratio * 0.5
             box = (int(round(center_x - half_w)), int(round(center_y - half_h)), int(round(center_x + half_w)), int(round(center_y + half_h)))
             img = origin.crop(box)
+            if resize:
+                img = img.resize((img_size_width, img_size_height), resize_algorithm)
             pairs.append((img, kind))
         for pair in pairs:
             yield pair
 
 def sealion_cnn():
-    inputs = Input((1, 512, 512))
+    inputs = Input((1, 64, 64))
     conv1 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(inputs)
     conv1 = Dropout(0.2)(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2), data_format='channels_first')(conv1)
@@ -72,7 +75,7 @@ def load_model(path, model):
 ############################
 if __name__ == '__main__':
     train_path = "/home/sleepywyn/Dev/GitRepo/Sealion/data/validate.txt"
-    train_gen = sealion_gen(train_path)
+    train_gen = sealion_gen(train_path, resize=True)
     img, label = train_gen.next()
     print img.size
     print label
